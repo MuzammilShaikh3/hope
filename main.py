fade3b66d0322416bfa79429cd0ed5128a559de7
MuzammilShaikh3 <30564975+MuzammilShaikh3@users.noreply.github.com>
2024-01-04 17:21:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import smtplib
 
 app = Flask(__name__, template_folder='template')
 app.secret_key = 'fdafadfqaerhgu'
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'Mroot@396'
+# app.config['MYSQL_DB'] = 'co'
+
 app.config['MYSQL_HOST'] = 'sql12.freemysqlhosting.net'
 app.config['MYSQL_USER'] = 'sql12674420'
 app.config['MYSQL_PASSWORD'] = 'fyVaeJ2Xug'
@@ -16,12 +21,13 @@ app.config['MYSQL_DB'] = 'sql12674420'
 mysql = MySQL(app)
  
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
         username = request.form['username']
         password = request.form['password']
+        email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM login2 WHERE username = % s AND pass = % s', (username, password ))
         loginstd = cursor.fetchone()
@@ -29,18 +35,18 @@ def login():
             session['loggedin'] = True
             session['id'] = loginstd['id']
             session['username'] = loginstd['username']
-            sendemail ="hopehelps36@gmail.com"
-            rec_email= "muzammilshaikhmohammed@gmail.com"
-            print(rec_email)
+            # sendemail ="hopehelps36@gmail.com"
+            
+            print(email)
             password= "Zuber123"
-            message ="Hey, this was sent using python"
+            message ="You just Logged Into HOPE"
 
-            server =smtplib.SMTP('smtp.gmail.com', 587)
-            server.starttls ()
-            server.login (sendemail, password)
+            # server =smtplib.SMTP('smtp.gmail.com', 587)
+            # server.starttls ()
+            # server.login (sendemail, password)
             print ("Login success")
-            server.sendmail (sendemail, rec_email, message)
-            print ("Email has been sent to ", rec_email)
+            # server.sendmail (sendemail, email, message)
+            # print ("Email has been sent to ", email)
             return render_template('index.html', username= username)
         else:
             msg = 'Incorrect username / password !'
@@ -50,6 +56,15 @@ def login():
 @app.route('/index')
 def home():
     return render_template('index.html')
+
+@app.route('/')
+def parallax():
+    return render_template('parallax.html')
+
+@app.route('/pop')
+def pop():
+    return render_template('pop.html')
+
 
 @app.route('/stress')
 def stress():
@@ -63,9 +78,6 @@ def Depression():
 def Anxiety():
     return render_template('Anxiety.html')
 
-@app.route('/donate')
-def donate():
-    return render_template('donate.html')
 
 @app.route('/story')
 def story():
@@ -79,6 +91,10 @@ def tasks():
 def contact():
     return render_template('contact.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
 
 @app.route('/logout')
 def logout():
@@ -88,7 +104,7 @@ def logout():
     return redirect(url_for('login'))
   
 @app.route('/register', methods =['GET', 'POST'])
-def signup():
+def register():
     msg = ''
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form :
         username = request.form['username']
@@ -108,6 +124,18 @@ def signup():
         else:
              cursor.execute('INSERT INTO login2 VALUES (NULL, % s, % s, % s)', (username, password, email, ))
              mysql.connection.commit()
+             sendemail ="hopehelps36@gmail.com"
+            
+             print(email)
+             password= "Zuber123"
+             message ="You have successfully registered to HOPE!"
+
+             server =smtplib.SMTP('smtp.gmail.com', 587)
+             server.starttls ()
+             server.login (sendemail, password)
+             print ("Login success")
+             server.sendmail (sendemail, email, message)
+             print ("Email has been sent to ", email)
              msg = 'You have successfully registered !'
     elif request.method == 'POST':
         msg = 'Please fill out the form !'
